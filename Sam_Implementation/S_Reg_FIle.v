ed309394620766bd479fa8cbaa17b11dffd28295
Sam-R-Taylor <samrtayloradm@gmail.com>
2020-11-27 19:52:22 +0000

    Fixed

--- Sam_Implementation/S_Reg_FIle.v
+++ Sam_Implementation/S_Reg_FIle.v
@@ -17,49 +17,54 @@ always @ (posedge sen)
 begin
     if(EN == 1)
         begin
-        if(rst == 1) // if at reset
-        begin 
-        for(i = 0 ; i <16; i = i+1)begin
-            S_Register_File[i] = 32'h0;
-        end
-
-        read_data_1 = 32^hx
-
-        end
-
-        else if (rst == 0) //if not at reset
-
-        begin
+            if(rst == 1) // if at reset
         
-        case({RD,WR})
-        
-        2'b00: begin //Nothing going on
-            
-        end
-        2'b01 begin //WR ACTIVE
-            S_Register_File[write_address] = write_data;
-        end
-        2'b10 begin //RD ACTIVE
-            read_data_1 = S_Register_File[read_address_1]
-            read_data_2 = S_Register_File[read_address_2]
-        end
-        2'b11 begin //BOTH RD AND WR ACTIVE
-             read_data_1 = S_Register_File[read_address_1]
-             read_data_2 = S_Register_File[read_address_2]
+                begin 
+                    for(i = 0 ; i <32; i = i+1)
+                    
+                    begin
+                    S_Register_File[i] = 32'h0;
+                    end
 
-             S_Register_File[write_address] = write_data;
-            
-        end
-        default: begin //IF UNDEFINED
-        end
-        endcase
+                    read_data_1 = 32^hx
 
-        end
+                end
 
-        else;
-        end
-else;
+            else if (rst == 0) //if not at reset
 
+                begin
+                
+                    case({RD,WR})
+                    
+                    2'b00: 
+                        begin //Nothing going on
+                            
+                        end
+                    2'b01 
+                        begin //WR ACTIVE
+                            S_Register_File[write_address] = write_data;
+                        end
+                    2'b10 
+                        begin //RD ACTIVE
+                            read_data_1 = S_Register_File[read_address_1]
+                            read_data_2 = S_Register_File[read_address_2]
+                        end
+                    2'b11 
+                        begin //BOTH RD AND WR ACTIVE
+                            read_data_1 = S_Register_File[read_address_1]
+                            read_data_2 = S_Register_File[read_address_2]
+
+                            S_Register_File[write_address] = write_data;
+                            
+                        end
+                    
+                    default: begin //IF UNDEFINED
+                    endcase
+                end
+
+            else;
+        end
+    else;
 end
 
 endmodule
