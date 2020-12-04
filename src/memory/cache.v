ab82c2c2d2ecd9272012f9dfa93d5255d3fe2c1e
Timothy Moores <info@tmoor.es>
2020-12-04 14:25:25 +0000

    formatting and comments

--- src/memory/cache.v
+++ src/memory/cache.v
@@ -1,5 +1,4 @@
-//`include "src/memory/util.vh"
-`include "util.vh"
+`include "src/memory/util.v"
 /*
   I've done [weird amount] of cache presently however this is easy to change.
   I need to look more into what an optimal amount is.
@@ -25,6 +24,7 @@ module cache (
     output logic[3:0] byteenable,
     input logic[31:0] readdata
   );
+
   //  Cache signals //
   logic miss = 0;
   logic[3:0][49:0] cache[4095:0]; // We can change size easily enough, size is weird because tag...
@@ -33,26 +33,28 @@ module cache (
   wire[17:0] tag;
   
   // Signals from CPU interface //
-  //logic cpu_write, cpu_read, cpu_wait;
+  logic cpu_write, cpu_read, cpu_wait;
   // more to come
   
   assign offset = address[1:0];
   assign cache_address = address[13:2]; // address % 12 ignoring offset
   assign tag = address[31:14];
 
+  // Do I need to be more explicit with time here? (ie are we assuming cache is instant?)
+
   always @(posedge write) `do (
-    $display("writing %h to %h, tag: %h", writedata, address, tag);
     waitrequest <= 1;
     cache[cache_address][3] <= cache[cache_address][2];
     cache[cache_address][2] <= cache[cache_address][1];
     cache[cache_address][1] <= cache[cache_address][0];
     cache[cache_address][0] <= {tag, writedata};
-    $display("Wrote %h to cache", cache[cache_address][0]);
     waitrequest <= 0;
   )
 
+  // It's probably wise to change this from a ternary to a MUX
+  // Z (high impedance) is synthesisable but I don't think it's best practise?
+
   always @(posedge read) `do (
-    $display("reading %h, looking for %h", address, tag);
     waitrequest <= 1;
     readdata <= cache[cache_address][0][49:32] == tag?
       cache[cache_address][0][31:0] : cache[cache_address][1][49:32] == tag?
@@ -64,7 +66,6 @@ module cache (
   )
   
   always @(posedge miss) `do (
-    $display("miss on %h", address);
     // Look in RAM for read address
   )
 endmodule
