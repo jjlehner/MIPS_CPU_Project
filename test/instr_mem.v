74ccf5685d0f4a8204667917b750c87ac208ede0
jy3119 <jy3119@imperial.ac.uk>
2020-12-14 06:43:25 +0900

    instr mem

--- test/instr_mem.v
+++ test/instr_mem.v
@@ -1,5 +1,4 @@
 module RAM_32x2048_delay0(
-input logic clk,reset, active,
 input logic[31:0] instr_address,
 output logic[31:0] instr_readdata,
 );
@@ -8,23 +7,10 @@ parameter RAM_INIT_FILE = "instrmem.txt"; /*this file is non existent yet*/
 reg [31:0] memory [8192:0];
 
 /*zero initialise*/
-initial begin
-integer i;
-/*zero initialise*/
-for(i=0; i<8192; i++) begin
-   memory[i] = 0;
-end
-/or_isit this/
-always @(posedge clk)
-    begin
-	if (reset) begin
-		for(i=0; i<8192; i++) begin
-		    memory[i] = 0;
-		end
-	end
-    end
+initial
+begin
 /*ram init*/
-$readmemh(RAM_INIT_FILE, memory);
+$readmemh(RAM_INIT_FILE, memory, 0, 8191);
 end
 assign instr_readdata = memory[instr_address];
 
